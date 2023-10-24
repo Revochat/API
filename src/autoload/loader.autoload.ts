cbf4c443221f14ea36bf5c012960f8f33e75a856
bylife <bylife@bylife.fr>
2023-10-24 23:23:39 +0200

    Added HTTP API Autoload

--- src/autoload/loader.autoload.ts
+++ src/autoload/loader.autoload.ts
@@ -7,10 +7,12 @@ import fs from "fs"
 import { config } from "../../config";
 import path from 'path';
 import { redefineSocket } from "./socket_struct.autoload";
+import express from "express";
 
 dotenv.config()
 
 export class Autoload { // This is the class that starts the server
+    static app: express.Express = express();
     static socket: Socket.Server = new Socket.Server(process.env.SOCKET_PORT ? Number(process.env.SOCKET_PORT) : 3000);
     static port: number;
     static baseDir = path.resolve(__dirname, "../socket");
@@ -77,6 +79,29 @@ export class Autoload { // This is the class that starts the server
     }
     
     
+    protected static autoloadRoutesFromDirectory(directory: string): void {
+        const httpMethods: (keyof express.Application)[] = ["get", "post", "put", "delete", "patch", "head", "options"];
+        const files = fs.readdirSync(directory);
+    
+        for (const file of files) {
+            const fullPath = path.join(directory, file);
+    
+            if (fs.statSync(fullPath).isDirectory()) {
+                Autoload.autoloadRoutesFromDirectory(fullPath);
+            } else if (file.endsWith('.ts')) {
+                const route = require(fullPath).default;
+                if (route && typeof route.run === 'function' && route.method && route.name) {
+                    const httpMethod = route.method.toLowerCase() as keyof express.Application;
+                    if (httpMethods.includes(httpMethod)) {
+                        Autoload.app[httpMethod](`/api${route.name}`, route.run);
+                        Logger.info(`Loaded route ${route.method} /api${route.name}`);
+                    } else {
+                        Logger.warn(`Unknown HTTP method: ${route.method}`);
+                    }
+                }
+            }
+        }
+    }
 
 
     protected static autoloadFilesFromDirectory(directory: string): any[] { // This is the function that is recursively loading all sockets files from the directory socket
@@ -120,6 +145,14 @@ export class Autoload { // This is the class that starts the server
         DB_Connect().then(() => {
             Autoload.socket.on("connection", function (socket: Socket.Socket) {
                 Autoload.attachHandlersToSocket(socket);
+            });
+
+            Autoload.autoloadRoutesFromDirectory(path.join(__dirname, '../http'));
+
+            Autoload.app.listen(Autoload.port, () => {
+                Logger.beautifulSpace();
+                Autoload.logInfo();
+                Logger.beautifulSpace();
             });
             Logger.beautifulSpace()
             Autoload.logInfo()
