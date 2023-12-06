c36f6f328892fb1bd53c3cb207c1d794a42b1b48
Luxchar <luccharlopeau@gmail.com>
2023-12-06 15:55:04 +0100

    fix addfriend

--- src/socket/user/friend/add.friend.ts
+++ src/socket/user/friend/add.friend.ts
@@ -12,56 +12,36 @@ export default {
             if(!data.friend_id) return socket.emit("user.friend.add", { error: "Missing friend_id" });
 
             const user = socket.revo.user; // get the user from the socket
-    
-            if(!user) return socket.emit("user.friend.add", { error: "You are not logged in" });
-    
+
+            const userDocument = await User.findOne({ user_id: user.user_id }); // find the user document
+            if (!userDocument) return socket.emit("user.friend.add", { error: "User not found" });
+
             const friend = await User.findOne({ user_id: data.friend_id }); // find the friend
             if(!friend) return socket.emit("user.friend.add", { error: "User not found" });
     
-            // check if user is already friend with the friend
-            const already_friend = user.friends.find((f: any) => f.user_id == friend.user_id);
-            if (already_friend) return socket.emit("user.friend.add", { error: "You are already friend with this user" });
-    
-            if(user.user_id == friend.user_id) return socket.emit("user.friend.add", { error: "You can't add yourself" });
-    
-            const friend_already_added = user.friends.find((f: any) => f.user_id == friend.user_id);
-            if(friend_already_added) return socket.emit("user.friend.add", { error: "This user is already your friend" });
-    
-            // check if the user already blocked the friend
-            const friend_blocked = user.blocked.find((f: any) => f.user_id == friend.user_id);
-            const user_blocked = friend.blocked.find((f: any) => f.user_id == user.user_id);
-            if(friend_blocked || user_blocked) return socket.emit("user.friend.add", { error: "You can't add this user" });
-    
-            // check if the user already sent a friend request to the friend
-            const friend_request_sent = user.friends_requests_sent.find((f: any) => f == friend.user_id);
-            if(friend_request_sent) return socket.emit("user.friend.add", { error: "You already sent a friend request to this user" });
-    
-            // check if the friend already sent a friend request to the user
-            const friend_request_received = user.friends_requests_received.find((f: any) => f == friend.user_id);
-            if(friend_request_received) {
-                // accept the friend request
-                user.friends_requests_received = user.friends_requests_received.filter((f: any) => f != friend.user_id);
-                friend.friends_requests_sent = friend.friends_requests_sent.filter((f: any) => f != user.user_id);
+            if(userDocument.friends.includes(friend.user_id)) return socket.emit("user.friend.add", { error: "You are already friends with " + friend.username });
 
-                const UserDocument = await User.findOne({ user_id: user.user_id }); // find the user document
-                if(!UserDocument) return socket.emit("user.friend.add", { error: "User not found" });
+            if(userDocument.friends_requests_sent.includes(friend.user_id)) return socket.emit("user.friend.add", { error: "You already sent a friend request to " + friend.username });
 
-                UserDocument.friends.push(friend.user_id);
-                await UserDocument.save();
-
-                friend.friends.push(user.user_id);
-                await friend.save();
-
-                return socket.emit("user.friend.add", { success: "You are now friend with " + friend.username }); // send a success message to the user
-            } else {
-                // send a friend request
-                user.friends_requests_sent.push(friend.user_id);
-                friend.friends_requests_received.push(user.user_id);
-                await user.save();
-                await friend.save();
+            if(userDocument.friends_requests_received.includes(friend.user_id)) { // if the user has a friend request from the friend
+                userDocument.friends_requests_received.splice(userDocument.friends_requests_received.indexOf(friend.user_id), 1); // remove the friend request from the user
+                friend.friends_requests_sent.splice(friend.friends_requests_sent.indexOf(user.user_id), 1); // remove the friend request from the friend
+                
+                userDocument.friends.push(friend.user_id); // add the friend to the user
+                friend.friends.push(user.user_id); // add the user to the friend
+                await userDocument.save(); // save the user document
+                await friend.save(); // save the friend document
+                socket.emit("user.friend.add", { success: "You are now friends with " + friend.username }); // send a success message to the user
+                return socket.to(friend.user_id).emit("user.friend.add", { success: "You are now friends with " + user.username }); // send a success message to the friend
+            } else { // if the user doesn't have a friend request from the friend
+                userDocument.friends_requests_sent.push(friend.user_id); // add the friend to the user's friend requests sent
+                friend.friends_requests_received.push(user.user_id); // add the user to the friend's friend requests received
+                await userDocument.save(); // save the user document
+                await friend.save(); // save the friend document
+                socket.emit("user.friend.add", { success: "You sent a friend request to " + friend.username }); // send a success message to the user
+                socket.to(friend.user_id).emit("user.friend.add", { success: user.username + " sent you a friend request" }); // send a success message to the friend
             }
-    
-            return socket.emit("user.friend.add", { success: "You sent a friend request to " + friend.username }); // send a success message to the user
+
         }
         catch (error) {
             console.log(error)
