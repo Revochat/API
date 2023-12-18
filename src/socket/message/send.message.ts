e0d3383731dc7ba70a88b0be2f65d153d27ebf7c
Luxchar <luccharlopeau@gmail.com>
2023-12-18 21:36:02 +0100

    missing try catch statement

--- src/socket/message/send.message.ts
+++ src/socket/message/send.message.ts
@@ -9,36 +9,42 @@ export default {
     name: "message.send",
     description: "Send a message",
     run: async function (socket: any, data: any, e : any) {
-        if(!socket.revo.logged) return socket.emit("message.send", { error: "You are not logged in" });
+        try {
+            if(!socket.revo.logged) return socket.emit("message.send", { error: "You are not logged in" });
 
-        if(!data) return socket.emit("message.send", "Please provide a message")
-        if(!data.message) return socket.emit("message.send", "Please provide a message")
-        if(!data.channel_id) return socket.emit("message.send", "Please provide a channel id")
-        
-        const user = socket.revo.user
-
-        const channel = await Channel.findOne({channel_id: data.channel_id}) // check if the channel exists
-        if(!channel) return socket.emit("message.send", "This channel doesn't exist")
-
-        // check if the user is in the channel
-        if(!channel.members.includes(user.user_id)) return socket.emit("message.send", "This user isn't in this channel")
-
-        const message = await Message.create({ // create the message
-            message_id: Date.now() + Math.floor(Math.random() * 100000),
-            user_id: user.user_id,
-            channel_id: channel.channel_id,
-            message: data.message,
-            created_at: new Date().toLocaleString()
-        })
-
-        Logger.info(`User ${user.username} sent a message to ${user.username} in channel ${channel.channel_id}`)
-
-        user.password = undefined // remove the password from the user object
-        user.token = undefined // remove the token from the user object
-
-        socket.emit("message.send", {message, user}) // send the message to the sender
-        socket.to(channel.channel_id).emit("message.send", {message, user}) // send the message to the channel
-        
-        return socket
+            if(!data) return socket.emit("message.send", "Please provide a message")
+            if(!data.message) return socket.emit("message.send", "Please provide a message")
+            if(!data.channel_id) return socket.emit("message.send", "Please provide a channel id")
+            
+            const user = socket.revo.user
+    
+            const channel = await Channel.findOne({channel_id: data.channel_id}) // check if the channel exists
+            if(!channel) return socket.emit("message.send", "This channel doesn't exist")
+    
+            // check if the user is in the channel
+            if(!channel.members.includes(user.user_id)) return socket.emit("message.send", "This user isn't in this channel")
+    
+            const message = await Message.create({ // create the message
+                message_id: Date.now() + Math.floor(Math.random() * 100000),
+                user_id: user.user_id,
+                channel_id: channel.channel_id,
+                message: data.message,
+                created_at: new Date().toLocaleString()
+            })
+    
+            Logger.info(`User ${user.username} sent a message to ${user.username} in channel ${channel.channel_id}`)
+    
+            user.password = undefined // remove the password from the user object
+            user.token = undefined // remove the token from the user object
+    
+            socket.emit("message.send", {message, user}) // send the message to the sender
+            socket.to(channel.channel_id).emit("message.send", {message, user}) // send the message to the channel
+            
+            return socket
+        }
+        catch (error) {
+            Logger.error(error)
+            return socket.emit("message.send", { error: "An error occured" });
+        }
     }
 }
