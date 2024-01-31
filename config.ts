7b202fc37bc89c2dd3bd61b739bcd35d2875af0a
Luxchar <luccharlopeau@gmail.com>
2024-01-31 09:01:42 +0100

    added more support for .env

--- config.ts
+++ config.ts
@@ -4,13 +4,14 @@ dotenv.config();
 const BASE_URI = process.env.BASE_URI;
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const MONGO_HOST = process.env.MONGO_HOST;
+const MONGO_PORT = process.env.MONGO_PORT;
 
 export const config = { // This is the config file for the RevoAPI. You can change the port, the timeout, and the application name, version, description, and owners.
     application: {
         name: "RevochatAPI",
         version: "1.2.0",
         description: `Revochat is a chat application that allows you to chat with your friends securely and privately.
-        Let's hope one day we can make it big!
         `,
         owners : [
             "ByLife",
@@ -25,7 +26,7 @@ export const config = { // This is the config file for the RevoAPI. You can chan
     },
     mongo: {
         username: MONGO_USERNAME,
-        url: (process.env.MONGO_URL?.replace("<USERNAME>", MONGO_USERNAME!).replace("<PASSWORD>", MONGO_PASSWORD!))!,
+        url: (process.env.MONGO_URL?.replace("<USERNAME>", MONGO_USERNAME!).replace("<PASSWORD>", MONGO_PASSWORD!).replace("<HOST>", MONGO_HOST!).replace("<PORT>", MONGO_PORT!) || "mongodb://localhost:27017/Revochat"),
     },
     api : {
         url : BASE_URI,
