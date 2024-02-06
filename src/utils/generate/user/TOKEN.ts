edeebcccb4a8c72b8612eb74bc1a38a500724c23
Luxchar <luccharlopeau@gmail.com>
2024-02-06 18:58:57 +0100

    fixed token creation duplicate on register

--- src/utils/generate/user/TOKEN.ts
+++ src/utils/generate/user/TOKEN.ts
@@ -1,5 +1,5 @@
 import { v4, v5 } from "uuid"
 
 export function generateUserToken () {
-    return (v5(Date.now().toString(), v4()).split("-").join("") + Date.now()).toUpperCase()
+    return (v5(Date.now().toString() + Math.random(), v4()).split("-").join("") + Date.now()).toUpperCase()
 }
